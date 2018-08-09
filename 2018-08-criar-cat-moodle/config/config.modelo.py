923539bb5a5bc2ed8b7954f26f7eb3432a76ab4e
Sergio Ricardo Bernardi Junior <sergiorbj93@gmail.com>
2018-08-09 18:46:24 -0300

    add config.modelo

---
+++ 2018-08-criar-cat-moodle/config/config.modelo.py
@@ -0,0 +1,7 @@
+class Config:
+    def __init__(self):
+
+        self.credenciais = {}
+        self.credenciais['token'] = 'token'
+        self.credenciais['domainname'] = 'dominio'
+        self.credenciais['functionname'] = 'nome_da_funcao_moodle'
