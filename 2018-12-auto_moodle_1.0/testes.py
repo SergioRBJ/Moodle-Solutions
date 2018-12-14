96db939d720818edc9e0aeeef74a51da0754a7f5
Sérgio Bernardi Junior <sergio.junior-plu@parceiros-kroton.com.br>
2018-12-14 15:56:07 -0200

    add testes no gitignore

--- 2018-12-auto_moodle_1.0/testes.py
+++ 2018-12-auto_moodle_1.0/testes.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 from src.acao import Acao
 import requests
+from config.conexao import DbConnect
 from config.config import Config
 from pprint import pprint
 from src.alocaGrupo import Grupo
@@ -24,59 +25,28 @@ for nome in dados2:
 for dadosT1 in dados1:
     if dadosT1['name'] not in listaNomes:
         print(dadosT1)'''
+listaTeste = []
+db = DbConnect().mysqlConnect()
+cursor = db.cursor()
 
-var = '2019U1230074JAN'
+query = """ SELECT u.username AS RA, u.institution AS POLO, c.fullname AS CURSO  FROM moodle.mdl_user u
+            INNER JOIN mdl_role_assignments ra ON ra.userid = u.id
+            INNER JOIN mdl_context cx ON cx.id = ra.contextid AND cx.contextlevel =50
+            INNER JOIN mdl_course c ON c.id = cx.instanceid
+            WHERE c.category = '1'
+            AND c.visible = '1' """
 
-var2 = 'U1230074'
+cursor.execute(query)
+'''resultado = cursor.fetchall()'''
 
-if (var[4:12] == var2):
-    print('ok')
+for (RA, POLO, DISC) in cursor:
+    listaTeste.append({'RA':RA, 'POLO':POLO, 'DISC': DISC})
 
-class Duplica:
-    def duplicaDisc(self):
-        config = Config()
+cursor.close()
+db.close()
 
-        '''Resgata todas as disciplinas'''
-
-        serverUrlDisc = config.dominio + "/webservice/rest/server.php" + "?wstoken=" + \
-                    config.duplicaToken + "&wsfunction=" + "core_course_get_courses" + "&moodlewsrestformat=" + config.formatoRest
-
-        response = requests.post(serverUrlDisc)
-        disciplinasAva = response.json()
-
-        discMatriz = []
-        discModelo = []
-
-        for disciplinasMatriz in disciplinasAva:
-            if str(disciplinasMatriz['categoryid']) == str(config.categoriaMatrizesEsp):
-                discMatriz.append([{'id': disciplinasMatriz['id'], 'idnumber': disciplinasMatriz['idnumber'],
-                                    'fullname': disciplinasMatriz['fullname']}])
-            if str(disciplinasMatriz['categoryid']) == str(config.categoriaUnija):
-                discModelo.append([{'id': disciplinasMatriz['id'], 'idnumber': disciplinasMatriz['idnumber'],
-                                    'fullname': disciplinasMatriz['fullname']}])
-        for discMatrizReg in discMatriz:
-            for discModeloReg in discModelo:
-                if discMatrizReg[0]['idnumber'] == discModeloReg[0]['idnumber'][4:12]:
-
-                    params = {'importfrom': discMatrizReg[0]['id'],
-                              'importto': discModeloReg[0]['id'],
-                              'deletecontent': '1',
-                              'options[0][name]': 'activities',
-                              'options[0][value]': '1',
-                              'options[1][name]': 'blocks',
-                              'options[1][value]': '0',
-                              'options[2][name]': 'filters',
-                              'options[2][value]': '0'
-                              }
-
-                    serverUrlImp = config.dominio + "/webservice/rest/server.php" + "?wstoken=" + \
-                                   config.duplicaToken + "&wsfunction=" + "core_course_import_course" + "&moodlewsrestformat=" + config.formatoRest
-
-                    response = requests.post(serverUrlImp, params)
-
-        return response
+pprint(listaTeste)
 
 
-aloca = Duplica()
-retorno = aloca.duplicaDisc()
-pprint(retorno)
+
+
