696119ab30ed0926275543193a42e7601c470689
Sérgio Bernardi Junior <sergio.junior-plu@parceiros-kroton.com.br>
2018-12-14 16:29:04 -0200

    alterações no arquivo de alocação de alunos em grupos

--- 2018-12-auto_moodle_1.0/src/alocaGrupo.py
+++ 2018-12-auto_moodle_1.0/src/alocaGrupo.py
@@ -1,5 +1,6 @@
 import requests
 from config.config import Config
+from config.conexao import DbConnect
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 '''remover warning por não verificar o SSL'''
@@ -11,79 +12,46 @@ class Grupo:
     def alocaGrupo(self):
 
         config = Config()
+        db = DbConnect().mysqlConnect()
+        cursor = db.cursor()
 
-        serverUrlDisc = config.dominio + "/webservice/rest/server.php" + "?wstoken=" + \
-                        config.alocaGrupoToken + "&wsfunction=" + "core_course_get_courses_by_field" \
-                        + "&moodlewsrestformat=" + config.formatoRest + '&field=category&value=' + str(config.categoriaUnija)
+        query = """ 
+                SELECT u.id AS ID_ALUNO, u.username as RA_ALUNO, u.institution AS POLO, c.fullname AS DISCIPLINA,
+                (SELECT gs.id FROM mdl_groups gs
+                LEFT JOIN mdl_groups_members gms ON gms.groupid = gs.id
+                WHERE gs.courseid = c.id AND gs.name = u.institution) AS GRUPO_ESPERADO_ID
+                
+                FROM moodle.mdl_user u
+                INNER JOIN mdl_role_assignments ra ON ra.userid = u.id
+                INNER JOIN mdl_context cx ON cx.id = ra.contextid AND cx.contextlevel =50
+                INNER JOIN mdl_course c ON c.id = cx.instanceid
+                INNER JOIN mdl_groups g ON g.courseid = c.id
+                LEFT JOIN mdl_groups_members gm ON gm.groupid = g.id AND gm.userid = u.id
+                
+                WHERE c.category = %s
+                AND c.visible = '1'
+                AND gm.id IS NULL
+                AND u.institution != '';
+         
+                """ % config.categoriaUnija
+        cursor.execute(query)
 
-        params = {'field': 'category', 'value': config.categoriaUnija}
+        listaAlunos = []
 
-        response = requests.get(serverUrlDisc, verify=False)
-        disciplinasAva = response.json()
-
-        listaAlunosGrupo = []
-
-        for disMod in disciplinasAva['courses']:
-
-            '''Resgata alunos matriculados na disciplina'''
-            params = {'courseid': disMod['id'],
-                      'options[0][name]': 'userfields',
-                      'options[0][value]': 'institution',
-                      'options[1][name]': 'userfields',
-                      'options[1][value]': 'username'
-                      }
-
-            serverUrlDisc = config.dominio + "/webservice/rest/server.php" + "?wstoken=" + \
-                            config.alocaGrupoToken + "&wsfunction=" + "core_enrol_get_enrolled_users" + "&moodlewsrestformat=" \
-                            + config.formatoRest + "&courseid=" + str(disMod['id']) + "&options[0][name]=userfields&options[0][value]=institution" \
-                            + "&options[1][name]=userfields&options[1][value]=username" + "&options[2][name]=onlyactive&options[2][value]=1"
-
-            response = requests.get(serverUrlDisc, verify=False)
-            alunosAva = response.json()
-
-            alunoLista=[]
-
-            '''Inserindo valores relevantes do json em lista '''
-            for aluno in alunosAva:
-                if len(aluno)>3:
-                    alunoLista.append(
-                        {'id': aluno['id'], 'institution': aluno['institution'], 'username': aluno['username']})
-
-            '''Resgata grupos já existentes na disciplina'''
-            params = {'courseid': disMod['id']}
+        for (ID_ALUNO, RA_ALUNO, POLO, DISCIPLINA, GRUPO_ESPERADO_ID) in cursor:
 
             serverUrlDisc = config.dominio + "/webservice/rest/server.php" + "?wstoken=" + \
-                            config.alocaGrupoToken + "&wsfunction=" + "core_group_get_course_groups" \
-                            + "&moodlewsrestformat=" + config.formatoRest + "&courseid=" + str(disMod['id'])
+                            config.alocaGrupoToken + "&wsfunction=" + "core_group_add_group_members" + "&moodlewsrestformat=" \
+                            + config.formatoRest + "&members[0][groupid]=" \
+                            + str(GRUPO_ESPERADO_ID) + "&members[0][userid]=" + str(ID_ALUNO)
 
             response = requests.get(serverUrlDisc, verify=False)
+            response.json()
 
-            gruposAva = response.json()
+            listaAlunos.append(
+                [{'nome': RA_ALUNO, 'grupo': POLO, 'disciplina': DISCIPLINA}])
 
-            '''Inserindo valores relevantes do json em lista '''
-            gruposLista = []
-
-            for grupos in gruposAva:
-                gruposLista.append({'id': grupos['id'], 'name': grupos['name'], 'disciplina': grupos['courseid']})
-
-            for grupo in gruposLista:
-                for aluno in alunoLista:
-                    if str(grupo['name']) == str(aluno['institution']):
-                        params = {'members[0][groupid]': grupo['id'],
-                                'members[0][userid]': aluno['id'],
-                                }
-
-                        serverUrlDisc = config.dominio + "/webservice/rest/server.php" + "?wstoken=" + \
-                                        config.alocaGrupoToken + "&wsfunction=" + "core_group_add_group_members" + "&moodlewsrestformat=" \
-                                        + config.formatoRest + "&members[0][groupid]=" \
-                                        + str(grupo['id']) + "&members[0][userid]=" + str(aluno['id'])
-
-                        response = requests.get(serverUrlDisc, verify=False)
-                        response.json()
-
-                        listaAlunosGrupo.append([{'nome': aluno['username'], 'grupo': grupo['name'], 'disciplina': grupo['disciplina']}])
-
-        return listaAlunosGrupo
+        return listaAlunos
 
 
 
